1b4527b1e90234dfe69d45f9093592cf8460a370
aymen <desuneyt@gmail.com>
2026-03-30 14:25:10 +0200

    Ajout Caddyfile pour Railway

--- mediatekformation.sql
+++ mediatekformation.sql
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `mediatekformation`
 --
-CREATE DATABASE IF NOT EXISTS `mediatekformation` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `mediatekformation`;
+
 
 -- --------------------------------------------------------
 
